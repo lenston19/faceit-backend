88e90d03119e90b86c997b996a60260f1e6203e1
ghettoDdOS <pen.egor2002@gmail.com>
2022-06-13 05:24:33 +0300

    fix permissions

--- config/settings/components/rest_framework.py
+++ config/settings/components/rest_framework.py
@@ -12,10 +12,9 @@ DATE_INPUT_FORMATS = [
 
 
 REST_FRAMEWORK = {
-    # "DEFAULT_PERMISSION_CLASSES": (
-    # ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": (
         "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
